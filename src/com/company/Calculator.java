071724166afcb1b2c79b989cd8dacdfe71ce617c
kumud96 <kumud.sharma0408@gmail.com>
2021-08-24 20:45:28 +0530

    Calculator.java

--- src/com/company/Calculator.java
+++ src/com/company/Calculator.java
@@ -1,2 +1,13 @@
-package com.company;public class Calculator {
+package com.company;
+
+public class Calculator {
+
+    public static void main(String[] args){
+
+        int a = 0;
+        int b = 2;
+        int c = a+b;
+        System.out.println(c);
+    }
+
 }
